c9b264758284b02893ebd136adb5c23cf76a1ed2
FahriyyaAlizada <fahriyyaalizada@gmail.com>
2024-09-12 19:08:26 +0400

    variables

---
+++ codes/javase/JavaSeCodes/src/main/Sep10.java
@@ -0,0 +1,46 @@
+package main;
+
+public class Sep10 {
+
+	public static void main(String[] args) {
+		int myAge = 30 ; //camelCase
+		//FirstClass //Pascal Case
+		//kebab-case //my-age
+		//snake case //my_age
+		byte number = 9; //8 bit
+		// 1 bit 0 or 1
+		// 8 bit all combination 2^8 = 256
+		char literal = 'A';
+		int num = (int)literal;
+		// System.out.println(num);
+		
+		
+		// Homework 1
+		System.out.println("HOMEWORK 1");
+		int age = 14;
+		System.out.println("age: " + age);
+		boolean hasHome = true;
+		System.out.println("hasHome: " + hasHome);
+		char studentGrade = 'D';
+		System.out.println("studentGrade: " + studentGrade);
+		double productPrice = 4.8;
+		System.out.println("productPrice: " + productPrice);
+		String studentName = "Faxriyya";
+		System.out.println("studentName: " + studentName);
+		
+		System.out.println("\n");
+		// Homework 2
+		System.out.println("HOMEWORK 2");
+		int newAge = 15;
+		System.out.println("newAge: " + newAge);
+		boolean hasJob = false;
+		System.out.println("hasJob: " + hasJob);
+		char newStudentGrade = 'F';
+		System.out.println("newStudentGrade: " + newStudentGrade);
+		double carPrice = 25000;
+		System.out.println("carPrice: " + carPrice);
+		String employeeName = "Faxriyya";
+		System.out.println("employeeName: " + employeeName);
+
+	}
+}
