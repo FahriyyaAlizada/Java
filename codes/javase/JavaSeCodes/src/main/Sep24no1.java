3bf2e256b504b019445ebb870016d7596b1c1e9d
FahriyyaAlizada <fahriyyaalizada@gmail.com>
2024-09-26 22:16:32 +0400

    class

---
+++ codes/javase/JavaSeCodes/src/main/Sep24no1.java
@@ -0,0 +1,46 @@
+package main;
+
+public class Sep24no1 {
+
+	public static void main(String[] args) {
+		// Sep24no2 book1 = new Sep24no2();
+		// book1.id = 1243;
+		// book1.name = "Chess";
+		// book1.author = "Stefan Zweig";
+		// book1.pageCount = 105;
+		// book1.description = "The Royal Game (also known as Chess Story) is a novella by the Austrian author Stefan Zweig written in 1941, the year before the author's death by suicide.";
+		// System.out.println(" Id: " + book1.id + "\n Name: " + book1.name + "\n Author: " + book1.author + "\n Page Number: " + book1.pageCount + "\n Description: " + book1.description);
+	
+		// Sep24no2 book2 = new Sep24no2();
+		// book2.id = 1791;
+		// book2.name = "Lord of the Flies";
+		// book2.author = "William Golding";
+		// book2.pageCount = 224;
+		// book2.description = "The plot concerns a group of British boys who are stranded on an uninhabited island and their disastrous attempts to govern themselves.";
+		// System.out.println("\n Id: " + book2.id + "\n Name: " + book2.name + "\n Author: " + book2.author + "\n Page Number: " + book2.pageCount + "\n Description: " + book2.description);
+		
+		// HOMEWORK 1
+		
+		// Sep24no2 car1 = new Sep24no2(3234, "Mercedes", "C-Class", "navy \n");
+		// Sep24no2 car2 = new Sep24no2(4126, "BMW", "5 Series", "white \n");
+		// Sep24no2 car3 = new Sep24no2(2118, "Ford", "Fusion", "dark red");
+		
+		//HOMEWORK 2
+		
+		// Sep24no2 person1 = new Sep24no2(5561, "Allen", "Straus", 36, "+123 456 7890 \n");
+		// Sep24no2 person2 = new Sep24no2(5562, "Rory", "Brown", 24, "+098 765 4321");
+		
+		// HOMEWORK 3
+		
+		Sep24no2 employee1 = new Sep24no2(8217, "Jack", "Watson", "+345 6821 7443", "18 George St.", 350000);
+		System.out.println("\n");
+		Sep24no2 employee2 = new Sep24no2(8218, "Max", "Smith", "+911 3527 2966", "98 Brook St.", 120000);
+		System.out.println("\n");
+		Sep24no2 employee3 = new Sep24no2(8219, "Alexandra", "Williams", "+399 2886 1130", "47 Bridge St.", 670000);
+		System.out.println("\n");
+		Sep24no2 employee4 = new Sep24no2(8220, "Camille", "Jones", "+635 2618 0874", "50 London St.", 810000);
+		
+		
+	}
+
+}
---
+++ codes/javase/JavaSeCodes/src/main/Sep24no2.java
@@ -0,0 +1,57 @@
+package main;
+
+public class Sep24no2 {
+	// int id;
+	// String name;
+	// String author;
+	// int pageCount;
+	// String description;
+	
+	// HOMEWORK 1
+	
+	// int id;
+	// String brand;
+	// String model;
+	// String color;
+	// public Sep24no2(int id, String brand, String model, String color) {
+	// 	this.id = id;
+	// 	this.brand = brand;
+	// 	this.model = model;
+	// 	this.color = color;
+	// 	System.out.println(" Id: " + id + "\n Brand: " + brand + "\n Model: " + model + "\n Color: " + color);
+	// }
+	
+	// HOMEWORK 2
+	
+	// int id;
+	// String name;
+	// String surname;
+	// int age;
+	// String phone;
+	// public Sep24no2(int id, String name, String surname, int age, String phone) {
+	// 	this.id = id;
+	// 	this.name = name;
+	// 	this.surname = surname;
+	// 	this.age = age;
+	// 	this.phone = phone;
+	// 	System.out.println(" Id: " + id + "\n Name: " + name + "\n Surname: " + surname + "\n Age: " + age  + "\n Phone: " + phone);
+	// }
+	
+	// HOMEWORK 3
+	int id;
+	String name;
+	String surname;
+	String phone;
+	String address;
+	int salary;
+	public Sep24no2(int id, String name, String surname, String phone, String address, int salary) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.phone = phone;
+		this.address = address;
+		this.salary = salary;
+		System.out.println(" Id: " + id + "\n Name: " + name + "\n Surname: " + surname + "\n Phone: " + phone  + "\n Address: " + address + "\n Salary: " + salary);
+	}
+	
+}
