ff39b0044a8e44bdf5dec88d356a23f1a1b63e84
FahriyyaAlizada <fahriyyaalizada@gmail.com>
2024-10-10 18:54:34 +0400

    reversing number

---
+++ codes/javase/JavaSeCodes/src/oct8/No1.java
@@ -0,0 +1,39 @@
+package oct8;
+
+import java.util.Scanner;
+
+public class No1 {
+
+	public static void main(String[] args) {
+		
+        // int number = 4;
+		// int cem = 0;
+		// for (int i = 1; i < number; i++) {
+		// 	if (number%i==0) {
+		// 		cem+=i;
+		// 	}
+		// 	
+		// }
+		// if (cem==number) {
+		// 	System.out.println("It is perfect number!");
+		// } else {
+		// 	System.out.println("It is not a perfect number...");
+		// }
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number...");
+        int num = scanner.nextInt();
+        String newNum = "";
+        if (num%10==0) {
+			num=num/10;
+		}
+        while (num!=0) {
+			int lastNum = num%10;
+			newNum += lastNum;
+			num=num/10;
+		}
+        System.out.println(newNum);
+
+	}
+
+}
