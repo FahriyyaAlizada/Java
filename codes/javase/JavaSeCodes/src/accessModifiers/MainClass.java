7d8dc122c4b44f3b74ac0585bf4a77bc8b553137
FahriyyaAlizada <fahriyyaalizada@gmail.com>
2024-10-31 21:12:08 +0400

    accessModifiers

---
+++ codes/javase/JavaSeCodes/src/accessModifiers/MainClass.java
@@ -0,0 +1,17 @@
+package accessModifiers;
+
+import accessModifiers_other.Book;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Book book1 = new Book();
+		book1.author = "Agatha Christie";
+		book1.page = 256;
+		book1.name = "The Big Four";
+		book1.releaseDate = 1927;
+
+	}
+
+}
---
+++ codes/javase/JavaSeCodes/src/accessModifiers_other/Book.java
@@ -0,0 +1,16 @@
+package accessModifiers_other;
+
+public class Book {
+
+	// default modifier - just in case in the same package
+	String author;
+	
+	// protected modifier - just in case in the same package
+	protected int page;
+	
+	// public modifier - can be used everywhere
+	public String name;
+	
+	// private modifiers - in the same class only
+	private int releaseDate;
+}
