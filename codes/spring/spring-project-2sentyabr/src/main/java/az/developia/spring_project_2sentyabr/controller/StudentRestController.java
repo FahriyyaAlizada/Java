323f53dcdd6b088b8279e86da338ad8a860ec28d
FahriyyaAlizada <fahriyyaalizada@gmail.com>
2025-03-08 17:27:23 +0400

    mysql

--- codes/spring/spring-project-2sentyabr/src/main/java/az/developia/spring_project_2sentyabr/controller/StudentRestController.java
+++ codes/spring/spring-project-2sentyabr/src/main/java/az/developia/spring_project_2sentyabr/controller/StudentRestController.java
@@ -1,8 +1,13 @@
 package az.developia.spring_project_2sentyabr.controller;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,9 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping(path="/api/student")
 public class StudentRestController {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@GetMapping
 	public List<Student> getStudents(){
@@ -37,5 +45,15 @@ public class StudentRestController {
 			throw new OurRuntimeException(br);
 		}
 		System.out.println(student);
+		
+		try {
+			Connection connection = dataSource.getConnection();
+			Statement st=connection.createStatement();
+			String query = "insert into students(name,surname) values('"+student.getName()+"','"+student.getSurname()+"')";
+			st.executeUpdate(query);
+			connection.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
